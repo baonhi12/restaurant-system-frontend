5bafa9308eb3b6f492015705ee1dd30af4b8e02b
baonhi12 <nhib.71939@gmail.com>
2025-03-10 16:34:02 +0700

    create table status page

--- src/pages/adminpage/TableStatus.js
+++ src/pages/adminpage/TableStatus.js
@@ -1,11 +1,53 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/admincomponent/Navbar';
 import '../../assets/css/Dashboard.css';
+import '../../assets/css/MenuManagement.css';
+import { IoIosSearch, IoMdNotifications, IoMdSettings } from "react-icons/io";
+import { FcBusinessman } from "react-icons/fc";
+import Badge from '@mui/material/Badge';
+import Button from '../../components/admincomponent/Button';
+import { IoIosAdd } from "react-icons/io";
 
 
 const TableStatus = () => {
     return (
-        <Navbar></Navbar>
+        <div className="dashboard-container">
+            <Navbar />
+
+            <div className="dashboard-content">
+                <div className="dashboard-header">
+                    <div class="input-group rounded">
+                        <input type="search" class="form-control rounded" placeholder="Search here ..." aria-label="Search" aria-describedby="search-addon" />
+                        <span class="input-group-text border-0" id="search-addon">
+                            <IoIosSearch />
+                        </span>
+                    </div>
+                    <div className="header-center">
+                        <Badge badgeContent={5} >
+                            <IoMdSettings className="icon" />
+                        </Badge>
+                        <Badge badgeContent={3} >
+                            <IoMdNotifications className="icon" />
+                        </Badge>
+                    </div>
+
+                    <div className="header-right">
+                        <p>Hello Manager</p>
+                        <FcBusinessman className="icon" />
+                    </div>
+                </div>
+
+                <div className="dashboard-title">
+                    <div className='dashboard-title-content'>
+                        <h2>Table Status</h2>
+                        <p>Here is our table status summary!</p>
+                    </div>
+                    <div className='dashboard-title-calendar'>
+                        {/* <Button><IoIosAdd className='dashboard-title-icon' /> New Food</Button> */}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
